c9177fb420baca82e968992d716561e574a952e5
jemo711 <jemojymw@gmail.com>
2025-12-29 18:20:38 +0300

    Update scribd.pyjemo

--- scribd.py
+++ scribd.py
@@ -10,7 +10,7 @@ import os
 os.chdir(sys.path[0])
 
 if len(sys.argv) ==1:
-	print 'Usage: sudo python scribd.py <link of scribd document>'
+	print 'Usage: sudo python scribd.py <https://www.scribd.com/document/805441271/%D9%85%D9%84%D8%B2%D9%85%D8%A9-%D8%A7%D9%84%D8%AA%D8%AF%D8%B1%D9%8A%D8%A8-%D8%A7%D9%84%D8%B9%D9%85%D9%84%D9%8A-%D9%84%D9%84%D8%B5%D9%81-%D8%A7%D9%84%D8%B3%D8%A7%D8%AF%D8%B3-%D8%A7%D9%84%D9%85%D9%87%D9%86%D9%8A-%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF-%D8%AC%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%AD%D8%AC%D8%A7%D9%85%D9%8A-1>'
 	print
 	print 'For selectable PDFs:'
 	print '- example: sudo python scribd.py https://www.scribd.com/document/55949937/33-Strategies-of-War'
